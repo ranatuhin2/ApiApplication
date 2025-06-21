a2e409c78970ff4c58b25dbe35ce5fcebaf18daa
Tuhin Rana Mondal <aimlesstuhin@gmail.com>
2025-06-21 13:02:34 +0530

    Updated Vendor

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ProductController;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Route;
-=======
->>>>>>> refs/remotes/origin/main
 
 
 Route::group(function(){
